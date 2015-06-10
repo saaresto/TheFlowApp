70daee148c22d96e02fd602056debd72c6de80b2
Ilya Issakin <ilya.issakin@gmail.com>
2015-06-10 15:59:27 +0600

    Added button to close popup in comments.

--- app/src/main/java/com/ilyaissakin/theflowapp/activity/DisqusActivity.java
+++ app/src/main/java/com/ilyaissakin/theflowapp/activity/DisqusActivity.java
@@ -27,6 +27,7 @@ public class DisqusActivity extends Activity {
     private WebView webView;
     private RelativeLayout root;
     private String disqusId;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +49,7 @@ public class DisqusActivity extends Activity {
 
             @Override
             public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+                menu.setGroupVisible(R.id.popup_group, true);
 
                 WebView loginView = new WebView(DisqusActivity.this);
                 WebSettings settings = loginView.getSettings();
@@ -58,14 +60,16 @@ public class DisqusActivity extends Activity {
                 loginView.setWebViewClient(new WebViewClient(){
                     @Override
                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                        if (url.contains("success"))
+                        if (url.contains("success")) {
                             view.setLayoutParams(new RelativeLayout.LayoutParams(0, 0));
+                            menu.setGroupVisible(R.id.popup_group, false);
+                        }
                         return false;
                     }
                 });
                 loginView.setWebChromeClient(new WebChromeClient());
 
-                loginView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 400));
+                loginView.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 root.addView(loginView);
                 root.setVisibility(View.VISIBLE);
 
@@ -123,6 +127,7 @@ public class DisqusActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_disqus, menu);
+        this.menu = menu;
         return true;
     }
 
@@ -134,6 +139,10 @@ public class DisqusActivity extends Activity {
         int id = item.getItemId();
 
         switch (id) {
+            case R.id.action_close_popup:
+                menu.setGroupVisible(R.id.popup_group, false);
+                root.setVisibility(View.GONE);
+                return true;
             case android.R.id.home:
                 onBackPressed();
                 return true;
